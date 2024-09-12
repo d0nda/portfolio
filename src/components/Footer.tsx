54c0cf1b28642d646d784c103ae89c39bc60306d
d0nda <dondahq@gmail.com>
2024-09-13 00:22:18 +0200

    dynamic year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ const Footer = () => {
         <footer className="border-black border-opacity-30 dark:border-white border-t dark:border-opacity-30">
             
                 <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row items-center justify-between">
-                    <p className="text-black dark:text-white text-md text-center sm:text-left sm:mr-auto">© 2024 Nkosinathi Mbokazi</p>
+                    <p className="text-black dark:text-white text-md text-center sm:text-left sm:mr-auto">© {new Date().getFullYear()} Nkosinathi Mbokazi</p>
                     <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 mb-3 justify-center sm:justify-start">
                         <Link href="https://www.linkedin.com/in/nkosinathimbokazi" target="_blank" className="hover:text-purple-400">
                             <FaLinkedin size={30} />
